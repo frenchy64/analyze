e184b08383f427733ea9c620f76c13c44ce4384e
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-12-30 20:30:37 +0800

    Start port of clojure.lang.Compiler's analyze

---
+++ src/analyze/port.clj
@@ -0,0 +1,40 @@
+(ns analyze.port)
+
+;; port of `analyze` method, line 6213, clojure.lang.RT
+
+(defn analyze-symbol
+  "Finds the var associated with sym"
+  [env sym]
+  (if (not (namespace sym))
+    (when-let [nm (-> env :locals sym)]
+      {:op :local :info {:name nm}})
+    (
+
+
+    ;; otherwise if (namespace sym) is a 
+  (let [ret {:env env :form sym}
+        lb (-> env :locals sym)]
+    (if lb
+      (assoc ret :op :var :info lb)
+      (assoc ret :op :var :info (resolve-existing-var env sym)))))
+
+
+(defn analyze
+  "Given an environment, a map containing {:locals (mapping of names to bindings), :context
+  (one of :statement, :expr, :return), :ns (a symbol naming the
+  compilation ns)}, and form, returns an expression object (a map
+  containing at least :form, :op and :env keys). If expr has any (immediately)
+  nested exprs, must have :children [exprs...] entry. This will
+  facilitate code walking without knowing the details of the op set."
+  ([env form] (analyze env form nil))
+  ([env form name]
+     (let [form (if (instance? clojure.lang.LazySeq form)
+                  (or (seq form) ())
+                  form)]
+       (cond
+        (symbol? form) (analyze-symbol env form)
+        (and (seq? form) (seq form)) (analyze-seq env form name)
+        (map? form) (analyze-map env form name)
+        (vector? form) (analyze-vector env form name)
+        (set? form) (analyze-set env form name)
+        :else {:op :constant :env env :form form}))))
