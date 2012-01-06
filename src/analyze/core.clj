a6f23781441b50ceeb21f23dd3e28b0638585c91
Jonas Enlund <jonas.enlund@gmail.com>
2012-01-06 23:33:14 +0200

    debuggin work. We can now analyze clojure.core.match

--- src/analyze/core.clj
+++ src/analyze/core.clj
@@ -12,7 +12,7 @@
                          Compiler$NewInstanceExpr Compiler$MetaExpr Compiler$BodyExpr Compiler$ImportExpr Compiler$AssignExpr
                          Compiler$TryExpr$CatchClause Compiler$TryExpr Compiler$C Compiler$LocalBindingExpr Compiler$RecurExpr
                          Compiler$MapExpr Compiler$IfExpr Compiler$KeywordInvokeExpr Compiler$InstanceFieldExpr Compiler$InstanceOfExpr
-                         Compiler$CaseExpr Compiler$Expr Compiler$SetExpr))
+                         Compiler$CaseExpr Compiler$Expr Compiler$SetExpr Compiler$MethodParamExpr))
   (:require [clojure.reflect :as reflect]
             [clojure.java.io :as io]
             [clojure.repl :as repl]
@@ -95,7 +95,7 @@
 (defmethod Expr->map Compiler$LetFnExpr
   [^Compiler$LetFnExpr expr env]
   (let [body (Expr->map (.body expr) env)
-        binding-inits (-> (doall (map BindingInit->vec (.bindingInits expr)))
+        binding-inits (-> (doall (map BindingInit->vec (.bindingInits expr) (repeat env)))
                         vec)]
     {:op :letfn
      :env env
@@ -400,12 +400,13 @@
 
 (defmethod Expr->map Compiler$NewInstanceExpr
   [^Compiler$NewInstanceExpr expr env]
-  (let [methods (doall (map ObjMethod->map (.methods expr) (repeat env)))]
+  (let [field (partial field-accessor Compiler$NewInstanceExpr)
+        methods (doall (map ObjMethod->map (field 'methods expr) (repeat env)))]
     {:op :new-instance-expr
      :env env
      :methods methods
-     :mmap (.mmap expr)
-     :covariants (.covariants expr)
+     :mmap (field 'mmap expr)
+     :covariants (field 'covariants expr)
      :tag (.tag expr)
      :children methods
      :Expr-obj expr}))
@@ -545,6 +546,11 @@
      :args args
      :children (concat loop-locals args)
      :Expr-obj expr}))
+
+(defmethod Expr->map Compiler$MethodParamExpr
+  [expr & args]
+  ;; TODO
+  {})
 
 (defmethod Expr->map :default
   [expr & args]
