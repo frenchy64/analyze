92917fd409e47ff33d13ffdb9b49e855039197d8
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-04-17 13:20:09 +0800

    Add 2 tests for ast macros

--- test/analyze/test/core.clj
+++ test/analyze/test/core.clj
@@ -1,1 +1,11 @@
-(ns analyze.test.core)
+(ns analyze.test.core
+  (:require [clojure.test :refer :all]
+            [analyze.core :refer :all]))
+
+(deftest test-ast
+  (is (= (-> (ast 1) :op)
+         :number)))
+
+(deftest test-ast-in-ns
+  (is (= (-> (ast-in-ns clojure.core 1) :op)
+         :number)))
