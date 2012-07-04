12e2c577f4d7fa9060a1706ae96d6f9a98a27c4e
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-07-05 05:27:30 +0800

    Add test for keyword-invoke

--- test/analyze/test/core.clj
+++ test/analyze/test/core.clj
@@ -9,3 +9,8 @@
 (deftest test-ast-in-ns
   (is (= (-> (ast-in-ns clojure.core 1) :op)
          :number)))
+
+(deftest test-keyword-invoke-kw
+  (is (= (-> (ast (let [a {}] (:abc a)))
+           :fexpr :methods first :body :exprs first :body :exprs first :kw :val)
+         :abc)))
