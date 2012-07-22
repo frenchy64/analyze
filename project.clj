edde8c2e096df997bc7118788f7a30f536448b42
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-07-22 21:16:52 +0800

    Release 0.1.7.1

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject analyze "0.1.7-SNAPSHOT"
+(defproject analyze "0.1.7.1"
   :description "Interface to Clojure Analyzer"
   :dependencies [[org.clojure/clojure "1.5.0-analyzenoreflect-SNAPSHOT"]])
