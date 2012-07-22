f04fea2b27cb44f87eea74453079e4e2ad18bf5b
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-07-22 22:24:39 +0800

    Increment version SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject analyze "0.1.7.1"
+(defproject analyze "0.1.7.2-SNAPSHOT"
   :description "Interface to Clojure Analyzer"
   :dependencies [[org.clojure.frenchy64/clojure "1.5.0-analyzenoreflect-SNAPSHOT"]])
