b23fa7f81bb6d6eeae1f2121ce0e2dbdf1987770
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-02-11 14:36:26 +0800

    0.3.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject analyze "0.2.7-SNAPSHOT"
+(defproject analyze "0.3.0"
   :description "Interface to Clojure Analyzer"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.trace "0.7.3"]])
